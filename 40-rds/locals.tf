792b46ed2ec8eaeb87d3e9d9163ac6425ed64c3d
makkenagithub <suresh.makkena@gmail.com>
2024-10-02 13:02:01 +0530

    expense RDS db

--- 40-rds/locals.tf
+++ 40-rds/locals.tf
@@ -1,8 +1,7 @@
 locals {
-    resource_name = "${var.project_name}-${var.env}-bastion"
+    resource_name = "${var.project_name}-${var.env}"
     # fetch the bastion sg id from aws param store
-    bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
+    mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
+    database_subnet_group_name = data.aws_ssm_parameter.database_subnet_group_name.value
 
-        # conver string list to list and use first subnet ID
-    public_subnet_id = split(",", data.aws_ssm_parameter.public_subnet_ids)[0]
 }
--- 40-rds/main.tf
+++ 40-rds/main.tf
@@ -1,47 +1,42 @@
 module "db" {
   source = "terraform-aws-modules/rds/aws"
 
-  identifier = "demodb"
+  identifier = local.resource_name  #expense-dev
 
   engine            = "mysql"
-  engine_version    = "5.7"
-  instance_class    = "db.t3a.large"
-  allocated_storage = 5
+  engine_version    = "8.0"
+  instance_class    = "db.t3.micro"
+  allocated_storage = 5 # 5 GB
 
-  db_name  = "demodb"
-  username = "user"
+  db_name  = "transactions"
+  username = "root"
+  manage_master_user_password = false
+  password = "ExpenseApp1"
   port     = "3306"
 
-  iam_database_authentication_enabled = true
+  #iam_database_authentication_enabled = true
 
-  vpc_security_group_ids = ["sg-12345678"]
+  vpc_security_group_ids = [local.mysql_sg_id]
 
-  maintenance_window = "Mon:00:00-Mon:03:00"
-  backup_window      = "03:00-06:00"
-
-  # Enhanced Monitoring - see example for details on how to create the role
-  # by yourself, in case you don't want to create it automatically
-  monitoring_interval    = "30"
-  monitoring_role_name   = "MyRDSMonitoringRole"
-  create_monitoring_role = true
-
-  tags = {
-    Owner       = "user"
-    Environment = "dev"
-  }
+  tags = merge(
+    var.common_tags,
+    var.rds_tags
+  )
 
   # DB subnet group
-  create_db_subnet_group = true
-  subnet_ids             = ["subnet-12345678", "subnet-87654321"]
+  #create_db_subnet_group = true
+  #subnet_ids             = ["subnet-12345678", "subnet-87654321"]
+
+  db_subnet_group_name = local.database_subnet_group_name
 
   # DB parameter group
-  family = "mysql5.7"
+  family = "mysql8.0"
 
   # DB option group
-  major_engine_version = "5.7"
+  major_engine_version = "8.0"
 
   # Database Deletion Protection
-  deletion_protection = true
+  #deletion_protection = true
 
   parameters = [
     {
