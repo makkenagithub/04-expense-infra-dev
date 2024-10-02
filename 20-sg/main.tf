aba4f85e9be19932b3a3d4edc79f4e42d957c903
Suresh Makkena <58598378+makkenagithub@users.noreply.github.com>
2024-10-02 17:50:13 +0530

    Update main.tf

--- 20-sg/main.tf
+++ 20-sg/main.tf
@@ -79,42 +79,42 @@ resource "aws_security_group_rule" "mysql_backend" {
 }
 
 
-# backend allowing connection on 8080 from instances attched to frontend sg
-resource "aws_security_group_rule" "backend_frontend" {
-  type              = "ingress"
-  from_port         = 8080
-  to_port           = 8080
-  protocol          = "tcp"
-  # accept connections from this source
-  source_security_group_id = module.frontend_sg.id
-  #cidr_blocks       = [aws_vpc.example.cidr_block]
-  #ipv6_cidr_blocks  = [aws_vpc.example.ipv6_cidr_block]
+# # backend allowing connection on 8080 from instances attched to frontend sg
+# resource "aws_security_group_rule" "backend_frontend" {
+#   type              = "ingress"
+#   from_port         = 8080
+#   to_port           = 8080
+#   protocol          = "tcp"
+#   # accept connections from this source
+#   source_security_group_id = module.frontend_sg.id
+#   #cidr_blocks       = [aws_vpc.example.cidr_block]
+#   #ipv6_cidr_blocks  = [aws_vpc.example.ipv6_cidr_block]
   
-  # security group to apply this rule to
-  security_group_id = module.backend_sg.id
-}
+#   # security group to apply this rule to
+#   security_group_id = module.backend_sg.id
+# }
 
-# frontend allowing connection on 80 from public
-resource "aws_security_group_rule" "frontend_public" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  # accept connections from this source
-  #source_security_group_id = module.frontend_sg.id
+# # frontend allowing connection on 80 from public
+# resource "aws_security_group_rule" "frontend_public" {
+#   type              = "ingress"
+#   from_port         = 80
+#   to_port           = 80
+#   protocol          = "tcp"
+#   # accept connections from this source
+#   #source_security_group_id = module.frontend_sg.id
 
-  cidr_blocks       = ["0.0.0.0/0"]
-  #ipv6_cidr_blocks  = [aws_vpc.example.ipv6_cidr_block]
+#   cidr_blocks       = ["0.0.0.0/0"]
+#   #ipv6_cidr_blocks  = [aws_vpc.example.ipv6_cidr_block]
   
-  # security group to apply this rule to
-  security_group_id = module.frontend_sg.id
-}
+#   # security group to apply this rule to
+#   security_group_id = module.frontend_sg.id
+# }
 
-# mysql allowing connection on 22 from bastion
+# mysql (RDS) allowing connection on 3306 from bastion
 resource "aws_security_group_rule" "mysql_bastion" {
   type              = "ingress"
-  from_port         = 22
-  to_port           = 22
+  from_port         = 3306  #22
+  to_port           = 3306  #22
   protocol          = "tcp"
   # accept connections from this source
   source_security_group_id = module.bastion_sg.id
@@ -166,7 +166,7 @@ resource "aws_security_group_rule" "bastion_public" {
   # accept connections from this source
   #source_security_group_id = module.ansible_sg.id
 
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"]   # usually we give our company IP here
   #ipv6_cidr_blocks  = [aws_vpc.example.ipv6_cidr_block]
   
   # security group to apply this rule to
@@ -174,21 +174,21 @@ resource "aws_security_group_rule" "bastion_public" {
 }
 
 
-# mysql allowing connection on 22 from ansible
-resource "aws_security_group_rule" "mysql_ansible" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  # accept connections from this source
-  source_security_group_id = module.ansible_sg.id
+# # mysql allowing connection on 22 from ansible
+# resource "aws_security_group_rule" "mysql_ansible" {
+#   type              = "ingress"
+#   from_port         = 22
+#   to_port           = 22
+#   protocol          = "tcp"
+#   # accept connections from this source
+#   source_security_group_id = module.ansible_sg.id
 
-  #cidr_blocks       = ["0.0.0.0/0"]
-  #ipv6_cidr_blocks  = [aws_vpc.example.ipv6_cidr_block]
+#   #cidr_blocks       = ["0.0.0.0/0"]
+#   #ipv6_cidr_blocks  = [aws_vpc.example.ipv6_cidr_block]
   
-  # security group to apply this rule to
-  security_group_id = module.mysql_sg.id
-}
+#   # security group to apply this rule to
+#   security_group_id = module.mysql_sg.id
+# }
 
 # backend allowing connection on 22 from ansible
 resource "aws_security_group_rule" "backend_ansible" {
